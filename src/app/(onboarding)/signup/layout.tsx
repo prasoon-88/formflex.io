b01cb735b628ef8da8956cd1900321945a812dac
Rohit-Kurmi-01 <rohitsingh626348@gmail.com>
2024-10-09 14:23:45 +0530

    second push

--- src/app/(onboarding)/signup/layout.tsx
+++ src/app/(onboarding)/signup/layout.tsx
@@ -6,40 +6,54 @@ export default function SignupLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>
-       <div className="grid grid-cols-12  h-full w-full bg-noble-black-700">
-            <div className="col-span-12 md:col-span-7  sm:col-span-7 lg:col-span-8">
-                <div className="">
-                    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" >
-                      {/* ------------------------------------------------Left Side---------------------------------------------------- */}
-                        <div className=" p-8 rounded-lg shadow-lg w-full max-w-4xl ">
-                            <div className="flex justify-between items-center mb-16">
-                                <div className="text-2xl font-bold">
-                                    <img className='h-6 w-6' src="/images/onboarding/Logo.png" alt="Logo" />
-                                </div>
-                                <Button variant="outline" size="lg" className="border-none stem-green-500"> Log In</Button>
-                                
-                            </div>
-
-                            {/* ----------------------------------------SignUp form----------------------------------------------- */}
-                            {children}
-                            
-
-                            {/* ----------------------------------------Footer Part----------------------------------------------- */}
-                            <div className="flex justify-between items-center mt-20 text-sm text-gray-500">
-                                <span>Formflex.io © 2024</span>
-                                <Link href="/" className="text-gray-500">Privacy Policy</Link>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="grid grid-cols-12  h-full w-full bg-noble-black-700">
+        <div className="col-span-12 md:col-span-7  sm:col-span-7 lg:col-span-8">
+          <div className="">
+            <div className="min-h-screen flex items-center justify-center bg-cover bg-center">
+              {/* ------------------------------------------------Left Side---------------------------------------------------- */}
+              <div className=" p-8 rounded-lg shadow-lg w-full max-w-4xl ">
+                <div className="flex justify-between items-center mb-16">
+                  <div className="text-2xl font-bold">
+                    <img
+                      className="h-6 w-6"
+                      src="/images/onboarding/Logo.png"
+                      alt="Logo"
+                    />
+                  </div>
+                  <Button
+                    variant="outline"
+                    size="lg"
+                    className="border-none stem-green-500"
+                  >
+                    Log In
+                  </Button>
                 </div>
-            </div>
-            {/* ---------------------------------------------------Side Image----------------------------------------------------------- */}
-            <div className="col-span-5 md:col-span-5 hidden sm:block lg:col-span-4 rounded-xl overflow-auto">
-                
-                <img className="h-screen w-full object-cover rounded-4xl md:rounded-xl" src="/images/onboarding/Illustration.png" alt="Illustration" />
-            </div>
-        </div>
-  </>;
-}
 
+                {/* ----------------------------------------SignUp form----------------------------------------------- */}
+                {children}
+
+                {/* ----------------------------------------Footer Part----------------------------------------------- */}
+                <div className="flex justify-between items-center mt-20 text-sm text-gray-500">
+                  <span>Formflex.io © 2024</span>
+                  <Link href="/" className="text-gray-500">
+                    Privacy Policy
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* ---------------------------------------------------Side Image----------------------------------------------------------- */}
+        <div className="col-span-5 md:col-span-5 hidden sm:block lg:col-span-4 rounded-xl overflow-auto">
+          <img
+            className="h-screen w-full object-cover rounded-4xl md:rounded-xl"
+            src="/images/onboarding/Illustration.png"
+            alt="Illustration"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/(onboarding)/signup/page.tsx
+++ src/app/(onboarding)/signup/page.tsx
@@ -5,44 +5,61 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 const Signup = () => {
-  return <div> 
-    {/* ----------------------------------------------------Heading--------------------------------------------------------------- */}
-          <h1 className="text-4xl mb-9 text-white font-sans font-normal">Connect with your team and bring your creative ideas to life.</h1>
-          {/* ----------------------------------------------------SignUp Form--------------------------------------------------------------- */}
-  <form>
-      <div className="grid grid-cols-2 gap-4 mb-9">
+  return (
+    <div>
+      {/* ----------------------------------------------------Heading--------------------------------------------------------------- */}
+      <h1 className="text-4xl mb-9 text-white font-sans font-normal">
+        Connect with your team and bring your creative ideas to life.
+      </h1>
+      {/* ----------------------------------------------------SignUp Form--------------------------------------------------------------- */}
+      <form>
+        <div className="grid grid-cols-2 gap-4 mb-9">
           <div>
-              <Label className="block text-sm mb-2 text-gray-600"> First name</Label>
-              <Input type="text" placeholder="First Name" />
+            <Label className="block text-sm mb-2 text-gray-600">
+              First name
+            </Label>
+            <Input type="text" placeholder="First Name" />
           </div>
           <div>
-              <Label className="block text-sm mb-2 text-gray-600"> Last name</Label>
-              <Input type="text" placeholder="Last name"  />
-              
+            <Label className="block text-sm mb-2 text-gray-600">
+              Last name
+            </Label>
+            <Input type="text" placeholder="Last name" />
           </div>
-      </div>
-      <div className="grid grid-cols-2 gap-4 mb-5 items-center">
+        </div>
+        <div className="grid grid-cols-2 gap-4 mb-5 items-center">
           <div>
-              <Label className="block text-sm mb-2 text-gray-600"> Password</Label>
-              <Input type="password" placeholder="Password"  />
-             
+            <Label className="block text-sm mb-2 text-gray-600">Password</Label>
+            <Input type="password" placeholder="Password" />
           </div>
           <div>
-              
-              <Label className="block text-sm mb-2 text-gray-600"> Repeat password</Label>
-              <Input type="password" placeholder="Repeat password" />
-              
+            <Label className="block text-sm mb-2 text-gray-600">
+              Repeat password
+            </Label>
+            <Input type="password" placeholder="Repeat password" />
           </div>
-      </div>
-{/* ----------------------------------------------------Terms Condition---------------------------------------------------------------------- */}
-      <div className="flex items-center mb-4">
-          <Checkbox className="border-gray-500"/>
-          <label className="text-sm text-white mx-2">I agree with <Link href="/" className="stem-green-500">Terms and conditions</Link></label>
-      </div>
-{/* ----------------------------------------------------Button---------------------------------------------------------------------- */}
-<Button variant="outline" size="lg" className="w-full py-2 rounded-xl bg-stem-green-500 text-black font-semibold hover:bg-black hover:text-white hover:border-none" > Create free account</Button>
-  </form>
-  </div>;
+        </div>
+        {/* ----------------------------------------------------Terms Condition---------------------------------------------------------------------- */}
+        <div className="flex items-center mb-4">
+          <Checkbox className="border-gray-500" />
+          <label className="text-sm text-white mx-2">
+            I agree with
+            <Link href="/" className="stem-green-500">
+              Terms and conditions
+            </Link>
+          </label>
+        </div>
+        {/* ----------------------------------------------------Button---------------------------------------------------------------------- */}
+        <Button
+          variant="outline"
+          size="lg"
+          className="w-full py-2 rounded-xl bg-stem-green-500 text-black font-semibold hover:bg-black hover:text-white hover:border-none"
+        >
+          Create free account
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
